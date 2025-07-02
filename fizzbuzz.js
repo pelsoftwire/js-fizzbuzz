4d73caf5beed33c11260ae39ab3394cef30bf8ac
Pelumi Babalola <Pelumi.Babalola@softwire.com>
2025-07-02 16:58:26 +0100

    added custom rules

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,29 @@
+// TODO: enable all rules if none are specified
+/*
+
+Command options
+
+newrule [number] [word] [wipe (optional)] [before (optional)]-[letter]
+
+creates a new rule that, if the number is divisible by [number], print [word] instead. if [wipe] is added, this rule will overwrite all previous rules (similar to 'Bong'). If [before]-[letter] is added, the added word will appear just before the first word beginning with [letter]
+
+3, 5, 7, 11, 13, 17
+
+enables the rules for 3, 5, 7, 1, 13, 17 respectively
+
+all
+
+enables all hardcoded rules
+ */
+
+// TODO: encode current default rules in 'newrule' syntax
+
 const readline = require("readline");
 
 // This is our main function
 
 // arguments in a dict for efficiency reasons
-function genOutput(num, args) {
+function genOutput(num, args, extraRules = {}) {
     var output = [];
 
     // --- RULES ---
@@ -21,6 +41,25 @@ function genOutput(num, args) {
     }
     if (num % 17 === 0 && args[17]) {
         output.reverse();
+    }
+
+    // --- NEW RULES ---
+    for (const [key, rule] of Object.entries(extraRules)) {
+        if (num % key === 0) {
+            if (rule.wipe) {
+                output = [rule.word]
+            } else if (rule.before) {
+                // this is duplicated code
+                let i = output.findIndex(function (x) { return x[0].toLocaleLowerCase() == rule.before.toLocaleLowerCase(); });
+                if (i === -1) {
+                    output.unshift(rule.word);
+                } else {
+                    output.splice(i,0, rule.word);
+                }
+            } else {
+                output.push(rule.word);
+            }
+        }
     }
 
     // --- OUTPUT ---
@@ -52,9 +91,64 @@ function fizzbuzz() {
         17: false
     }
 
+    var extraRules = {}
+
     // convert list into a dict for efficiency reasons
-    commandLineArgs.forEach(function (arg) {
-        if (arg === "all") {
+    var i = 0;
+    while (i < commandLineArgs.length) {
+        var arg = commandLineArgs[i];
+        if (arg === "newrule") {
+            var j = i + 1; // this iterator may be redundant
+            // create a new rule.
+            var divisor = 0;
+            const newRule = {
+                word: null,
+                wipe: false,
+                before: false
+            }
+            while (j < commandLineArgs.length) {
+                var rarg = commandLineArgs[j];
+                // stop if a new rule is encountered
+                if (rarg === "newrule") {
+                    break;
+                // set wipe if encountered (otherwise leave it as false)
+                } else if (rarg === "wipe") {
+                    newRule.wipe = true;
+                // allow assignment of 'before' only once
+                } else if (rarg.includes("before")) {
+                    if (newRule.before === false) {
+                        var letter = rarg.split("-")[1];
+                        newRule.before = letter;
+                    } else {
+                        console.error("'before' may only be assigned once in 'newrule'");
+                        process.exit();
+                    }
+                // allow assignment of number for rule only once
+                } else if (!isNaN(parseInt(rarg))) {
+                    if (divisor === 0) {
+                        divisor = parseInt(rarg);
+                    } else {
+                        console.error("divisor may only be assigned once in 'newrule'");
+                        process.exit();
+                    }
+                // if none of the above, then assume it is the word. Allow assignment only once
+                } else if (newRule.word === null) {
+                    newRule.word = rarg;
+                } else {
+                    console.error("Unknown error parsing 'newrule'");
+                    process.exit();
+                }
+                j++;
+            }
+            if (extraRules.hasOwnProperty(divisor)) {
+                console.error("Can only assign one newrule before number");
+                process.exit();
+            }
+
+            extraRules[divisor] = newRule;
+
+            i = j-1;
+        } else if (arg === "all") {
             Object.keys(commandLineDict).forEach(function (key) {
                 commandLineDict[key] = true;
             });
@@ -65,11 +159,12 @@ function fizzbuzz() {
             console.error("Unrecognized argument " + arg);
             process.exit();
         }
-    })
+        i++;
+    }
 
     reader.question("Please enter a number >> ", function(x) {
         for (var i = 1; i <= x; i++) {
-            console.log(genOutput(i, commandLineDict));
+            console.log(genOutput(i, commandLineDict, extraRules));
         }
         reader.close();
     });
